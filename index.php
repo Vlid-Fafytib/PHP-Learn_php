a45948746ce603b5c18a8c584a57fe3eb761c986
Vlid-Fafytib <fafytibvlid@gmail.com>
2018-08-13 19:34:18 +0300

    Learn php start

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP</title>
+</head>
+<body>
+    <h2>Hello!</h2>
+    <p>
+    <?php 
+        // $dat = date("d.m.y");
+        // $tm = date("h:i:s");
+        // echo "Текущая дата: $dat года <br/> \n";
+        // echo "Текущее время: $tm по серверному времени<br/> \n";
+        // echo "Квадараты и кубы первых 5 натуральных чисел";
+        // echo "<ul>\n";
+        // for ($i=0; $i < 6; $i++) { 
+        //     echo "<li>$i в квадрате = " . ($i * $i);
+        //     echo ", $i в кубе = " . ($i * $i * $i) . "</li>\n";
+        // }
+        
+    ?>
+</body>
+</html>
