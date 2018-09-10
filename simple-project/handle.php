84a7d7db8e93a74a9087412d1361457d0dbf505a
Vlid-Fafytib <fafytibvlid@gmail.com>
2018-09-10 19:43:11 +0300

    login

--- simple-project/handle.php
+++ simple-project/handle.php
@@ -9,6 +9,8 @@
     $day = $_GET['day'];
     $gender = $_GET['gender'];
     $birth = "19$year";
+    $login_pass = $_GET['pass3'];
+    $login_name = $_GET['logem'];
     if ($month < 10) {
         $birth .= "-0$month";
     } else{ 
@@ -19,20 +21,27 @@
     } else{ 
         $birth .= "-$day";
     }
-    
+
+    $login1 = mysqli_query($connection, "SELECT COUNT(*) AS 'check' FROM `users` WHERE `nickname` = '$login_name' AND `password` = '$login_pass'");
+    $login_fetched = mysqli_fetch_assoc($login1);
+    if ($login_fetched['check'] != 0){
+        echo "Здравствуйте дорогой ". $login_name;
+    } else {
+        echo "Вы ввели не верный логин или пароль!!!";
+    }
     $nicks = mysqli_query($connection, "SELECT * FROM `users` WHERE `nickname` = '$nick'");
     if ($_GET["pass1"] != $_GET["pass2"]) {
         echo "Ваши пароли на совпадают";
     } else if (!$nicks || mysqli_num_rows($nicks) != 0){
         echo "Данный ник не1м уже занят";
     } else{
-        $sql = "INSERT INTO `users` (`name`, `nickname`, `birth_date`, `gender`, `password`, `email`) VALUES ('$name', '$nick', '$birth', '$gender', '$pass', '$email')";
-        if (mysqli_query($connection, $sql)) {
-            echo "New record created successfully";
-        } else {
+    $sql = "INSERT INTO `users` (`name`, `nickname`, `birth_date`, `gender`, `password`, `email`) VALUES ('$name', '$nick', '$birth', '$gender', '$pass', '$email')";
+    if (mysqli_query($connection, $sql)) {
+        echo "New record created successfully";
+        } else {    
             echo "Error: " . $sql . "<br>" . mysqli_error($connection);
         }
-    
     }
+
 mysqli_close($connection);
 ?>
---
+++ simple-project/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Логин</h1>
+    <form action="handle.php" method="get">
+        <p><label>Введите имя пользователя: <input type="text" name="logem"></label></p>
+        <p><label>Введите пароль: <input type="password" name="pass3"></label></p>
+        <p><label><input type="submit" value="Отправить" name="login_go"></label></p>
+    </form>
+</body>
+</html>
